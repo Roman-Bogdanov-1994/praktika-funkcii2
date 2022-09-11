e5eecbe5bf37d2bc02333ca40f6508a2837b3d92
Roman <brv161@gmail.com>
2022-09-11 21:37:33 +0300

    Задача 5

---
+++ Задача 5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введиле два числа");
+Console.Write("a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+    Console.WriteLine("a^2 = b");
+else 
+   Console.WriteLine("a^2 = " + a * a);
