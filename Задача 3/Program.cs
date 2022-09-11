c6a0e98bb56d11337e59c694cc4791fe763b0a1b
Roman <brv161@gmail.com>
2022-09-11 21:03:01 +0300

    Задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,25 @@
+﻿int GRN1()//"Генератор случайных чисел"
+{
+    int n = new Random().Next(10, 100);
+    return n;
+}
+int GRN2()//"Генератор случайных чисел"
+{
+    int m = new Random().Next(1, 10);
+    return m;
+}
+
+void CN()//"Сравнение чисел"
+{
+    int n = GRN1();
+    int m = GRN2();
+    Console.Write(n);
+    Console.Write(", ");
+    Console.WriteLine(m);
+    if (n % m == 0)
+        Console.WriteLine("кратно");
+    else 
+        Console.Write("не кратно, остаток " + n % m);
+}
+
+CN();
