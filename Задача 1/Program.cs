8c8a364a20828426ca66a72ec02ef05878a56abf
Roman <brv161@gmail.com>
2022-09-11 19:42:38 +0300

    Задача 1

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GRN()//"Генератор случайных чисел
+{
+    int n = new Random().Next(10, 100);
+    return n;
+}
+
+void CN()
+{
+    int n = GRN();//Сравнение чисел
+    Console.WriteLine(n);
+    int a = n / 10;
+    int b = n % 10;
+    if (a > b)
+        Console.WriteLine(a);
+    else if (b > a)
+        Console.WriteLine(b);
+    else
+       Console.WriteLine("=");
+}
+
+CN();
