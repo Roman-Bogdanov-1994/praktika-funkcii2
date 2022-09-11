58ae304765433a781b7dce80d65504e4d29b195b
Roman <brv161@gmail.com>
2022-09-11 21:25:02 +0300

    Задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введиле число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("На что делим?");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+if (a % b % c == 0)
+    Console.WriteLine("Кратно, ответ " + a / b / c);
+else 
+   Console.WriteLine("Не кратно, ответ " + a / b / c + " Остаток " + a % b % c);
