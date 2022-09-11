9aec0a23068e56a19f5b8474bfdea41660ccc5f6
Roman <brv161@gmail.com>
2022-09-11 21:44:41 +0300

    Задача 5

--- Задача 5/Program.cs
+++ Задача 5/Program.cs
@@ -3,7 +3,13 @@ Console.Write("a = ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("b = ");
 int b = Convert.ToInt32(Console.ReadLine());
+// if (a * a == b)
+//     Console.WriteLine("a^2 = b");
+// else 
+//    Console.WriteLine("a^2 = " + a * a);
 if (a * a == b)
-    Console.WriteLine("a^2 = b");
-else 
-   Console.WriteLine("a^2 = " + a * a);
+     Console.WriteLine("a^2 = b");
+else if (b * b == a)
+     Console.WriteLine("b^2 = a");
+else     
+    Console.WriteLine("Ни одно число не квадрат другого");
