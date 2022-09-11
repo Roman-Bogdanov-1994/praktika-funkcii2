1d856a38e9260f8e7fae897c8bedec9117d2252b
Roman <brv161@gmail.com>
2022-09-11 20:18:51 +0300

    Задача 2

---
+++ Задача 2/Program.cs
@@ -0,0 +1,18 @@
+﻿int GRN()//"Генератор случайных чисел"
+{
+    int n = new Random().Next(100, 1000);
+    return n;
+}
+
+void DMN()//"Удаление среднего числа"
+{
+    int n = GRN();
+    Console.WriteLine(n);
+    int a = n / 100;
+    int b = n % 10;
+        Console.Write(a);
+        Console.Write(b);
+
+}
+
+DMN();
